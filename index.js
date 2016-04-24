5abf108b28e691f365bfc2310f2fb8d1c3b1576c
Maria Ines Parnisari <maineparnisari@gmail.com>
2016-04-24 17:27:57 -0300

    Fixed bug in paths

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-var cbu = require('../src/cbu');
-var cuit = require('../src/cuit');
-var phone = require('../src/phone');
+var cbu = require('./src/cbu');
+var cuit = require('./src/cuit');
+var phone = require('./src/phone');
 
 module.exports = {
   cbu: cbu,
