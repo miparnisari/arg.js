d1a82209fa8267f2fff48c0769b34b42fe901f7c
miparnisari <maineparnisari@gmail.com>
2017-09-09 21:15:17 -0700

    Remove gulp-shell. Add gulp-mocha

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,11 @@
 var gulp = require('gulp');
-var shell = require('gulp-shell');
+var mocha = require('gulp-mocha');
 var jsdoc = require('gulp-jsdoc3');
 
-gulp.task('test', shell.task([
-  'mocha'
-]));
-
+gulp.task('test', function() {
+  gulp.src(['./test/**/*.js'], {read: false})
+        .pipe(mocha({reporter: 'spec'}))
+})
 gulp.task('doc', function (cb) {
     gulp.src(['./src/**/*.js'], {read: false})
         .pipe(jsdoc(cb));
