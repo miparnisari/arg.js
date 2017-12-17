55f17352b182646e69d236d64a6dd42573a4b794
mparnisari <maineparnisari@gmail.com>
2017-12-17 14:54:07 -0800

    Fix for #11. Version 1.1

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 var cbu = require('./src/cbu');
 var cuit = require('./src/cuit');
 var phone = require('./src/phone');
+var document = require('./src/document');
 
 module.exports = {
   cbu: cbu,
   cuit: cuit,
-  phone: phone
+  phone: phone,
+  document: document
 };
