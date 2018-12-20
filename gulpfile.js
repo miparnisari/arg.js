7ffa7ef102091645625458e83915873bd7e4d3a3
Axel Navarro <navarroaxel@gmail.com>
2018-12-20 09:52:29 -0300

    Update gulp tasks to gulp@4 format

--- gulpfile.js
+++ gulpfile.js
@@ -2,14 +2,12 @@ var gulp = require('gulp');
 var mocha = require('gulp-mocha');
 var jsdoc = require('gulp-jsdoc3');
 
-gulp.task('test', function(done) {
-    gulp.src(['./test/**/*.js'], {read: false})
-        .pipe(mocha({reporter: 'spec'}))
-    done();
-});
-  
-gulp.task('doc', function (done) {
-    gulp.src(['./src/**/*.js'], {read: false})
-        .pipe(jsdoc());
-    done();
-});
+exports.test = function () {
+  return gulp.src(['./test/**/*.js'], {read: false})
+    .pipe(mocha({reporter: 'spec'}));
+};
+
+exports.doc = function () {
+  return gulp.src(['./src/**/*.js'], {read: false})
+    .pipe(jsdoc());
+};
